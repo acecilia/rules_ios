754ce529b28c2a4ab63ea78fb72d6d61a7d1a199
Andres Cecilia Luque <a.cecilia.luque@gmail.com>
2020-05-01 22:57:22 +0200

    Added example project demonstrating the module not found issue

---
+++ tests/mixed-source-import-swift/ios/MixedSourceFramework/Framework.swift
@@ -0,0 +1,14 @@
+import UIKit
+import SwiftLibrary
+
+/* Declaring extension Foo makes the framework build fail with the error:
+ * ./bazel-out/apl-ios_x86_64-fastbuild/bin/tests/mixed-source-import-swift/ios/MixedSourceFramework-Swift.h:184:9: fatal error: module 'SwiftLibrary' not found
+ * @import SwiftLibrary;
+ * ~~~~~~~^~~~~~~~~~~~
+ * 1 error generated.
+ *
+ * Commenting out extension Foo: the framework builds without any problem
+ */
+extension Foo { }
+
+public extension Bar { }
